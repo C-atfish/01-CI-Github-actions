b1bf02bff56a41f7f41d693508d0c30c9fe66c98
C-atfish <ec2-user@ip-172-31-46-229.eu-west-1.compute.internal>
2022-09-10 11:19:40 +0000

    broke

--- src/main/java/com/pgr301/exam/BankAccountController.java
+++ src/main/java/com/pgr301/exam/BankAccountController.java
@@ -27,7 +27,7 @@ public class BankAccountController implements ApplicationListener<ApplicationRea
         bankService.transfer(tx, fromAccount, toAccount);
     }
 
-    @PostMapg(path = "/account", consumes = "application/json", produces = "application/json")
+    @PostMappping(path = "/account", consumes = "application/json", produces = "application/json")
     public ResponseEntity<Account> updateAccount(@RequestBody Account a) {
         bankService.updateAccount(a);
         return new ResponseEntity<>(a, HttpStatus.OK);
